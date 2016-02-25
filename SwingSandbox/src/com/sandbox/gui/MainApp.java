84672ac587570c0303f731021a075a55becad35e
Mauro Gómez <mauricio.gomez@gmail.com>
2016-02-25 15:09:59 -0300

    25-02

--- SwingSandbox/src/com/sandbox/gui/MainApp.java
+++ SwingSandbox/src/com/sandbox/gui/MainApp.java
@@ -6,13 +6,24 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JDesktopPane;
+import javax.swing.JFileChooser;
+
 import java.awt.BorderLayout;
 import javax.swing.JScrollPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JInternalFrame;
+import javax.swing.JMenuItem;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.Action;
 
 public class MainApp {
 
 	private JFrame frame;
+	private final Action action = new SwingAction();
+	private final Action action_1 = new SwingAction_1();
 
 	/**
 	 * Launch the application.
@@ -51,6 +62,10 @@ public class MainApp {
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
+		JMenuItem mntmOpenFile = new JMenuItem("Open File...");
+		mntmOpenFile.setAction(action);
+		mnFile.add(mntmOpenFile);
+		
 		JScrollPane scrollPane = new JScrollPane();
 		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 		
@@ -64,16 +79,47 @@ public class MainApp {
 		//internalFrame.setVisible(true);
 
 		IfEditor ifEditor = new IfEditor();
-		ifEditor.setBounds(171, 73, 189, 72);
+		ifEditor.setBounds(229, 23, 189, 72);
 		ifEditor.setResizable(true);
 		desktopPane.add(ifEditor);
 		ifEditor.setVisible(true);
 
 		IfMultiEditor ifMultiEditor = new IfMultiEditor();
+		ifMultiEditor.setLocation(53, 70);
 		//ifMultiEditor.setBounds(171, 73, 189, 72);
 		ifMultiEditor.setResizable(true);
 		desktopPane.add(ifMultiEditor);
 		ifMultiEditor.setVisible(true);
 
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Open File...");
+			putValue(SHORT_DESCRIPTION, "Open a File");
+		}
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser chooser = new JFileChooser();
+			FileNameExtensionFilter filter = new FileNameExtensionFilter(
+					"JPG & GIF Images", "jpg", "gif");
+			chooser.setFileFilter(filter);
+			chooser.setCurrentDirectory(new File("c:\\"));
+			int returnVal = chooser.showOpenDialog(frame);
+			if(returnVal == JFileChooser.APPROVE_OPTION) {
+				System.out.println("You chose to open this file: " +
+						chooser.getSelectedFile().getName());
+				System.out.println("Path: " +
+						chooser.getSelectedFile().getParent());
+				
+				
+			}
+		}
+	}
+	private class SwingAction_1 extends AbstractAction {
+		public SwingAction_1() {
+			putValue(NAME, "SwingAction_1");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
